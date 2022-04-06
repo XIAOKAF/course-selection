fc371e450f67b2b8bd1615c4c533f1ddb61f99c2
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-06 19:56:45 +0800

    refactor:定义Student结构体

---
+++ model/student.go
@@ -0,0 +1,14 @@
+package model
+
+type Student struct {
+	StudentId   int
+	UnifiedCode string
+	StudentName string
+	Gender      int
+	Grade       string
+	Class       string
+	Password    string
+	Department  string
+	Major       string
+	Image       string
+}
