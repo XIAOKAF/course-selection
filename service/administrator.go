99e49d703cdd6b353923676921bc96f80fda2ca5
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-11 17:54:26 +0800

    feat:新增高级管理员登录的功能

---
+++ service/administrator.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"course-selection/dao"
+	"course-selection/model"
+)
+
+func AdministratorLogin(administrator model.Administrator) (error, string) {
+	err, password := dao.AdministratorLogin(administrator)
+	if err != nil {
+		return err, password
+	}
+	return nil, password
+}
