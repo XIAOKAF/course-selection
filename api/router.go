c844d91686b665e011b2b99984f4bc6d881a91c3
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-11 19:54:53 +0800

    refactor:连接redis

--- api/router.go
+++ api/router.go
@@ -1,9 +1,19 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func InitEngine() {
 	engine := gin.Default()
-
+	engine.Use(Authorization)
+	//高级管理员
+	administratorGroup := engine.Group("/administrator")
+	{
+		administratorGroup.POST("/login", administratorLogin) //管理员登录
+		administratorGroup.POST("/remember")
+	}
+	//学生
+	//教师
 	engine.Run(":8080")
 }
--- dao/dao.go
+++ dao/dao.go
@@ -1,12 +1,14 @@
 package dao
 
 import (
+	"github.com/go-redis/redis"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
 )
 
 var DB *gorm.DB
+var RDB *redis.Client
 
 func InitDB() {
 	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
@@ -16,4 +18,17 @@ func InitDB() {
 		return
 	}
 	DB = db
+}
+
+func InitRDB() {
+	RDB = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err := RDB.Ping().Result()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 }
