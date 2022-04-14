39b76832319ed4f9307ffd7f17f8295343953b8f
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-14 20:35:27 +0800

    refactor:修改对应文件的格式

--- model/RBAC.go
+++ model/RBAC.go
@@ -1,27 +1,27 @@
 package model
 
-type Rule struct {
-	//id决定了Rule的优先级，从0到3，优先级增高
+type Role struct {
+	//id决定了Role的优先级，从0到3，优先级增高
 	//当角色拥有多个优先级，采用最高优先级
-	Id int `json:"id"`
+	Id int `yaml:"id"`
 	*Resource
 	*Permission
 }
 
 type Resource struct {
 	//资源的host
-	Host string `json:"host"`
+	Host string `yaml:"host"`
 	//资源的path
-	Path string `json:"path"`
+	Path string `yaml:"path"`
 	//资源的method
-	Method string `json:"method"`
+	Method string `yaml:"method"`
 }
 
 type Permission struct {
 	//允许访问资源的角色
-	AuthorizedRoles []string `json:"authorized_roles"`
+	AuthorizedRoles []string `yaml:"authorized_roles"`
 	//不允许访问资源的角色
-	ForbiddenRoles []string `json:"forbidden_roles"`
+	ForbiddenRoles []string `yaml:"forbidden_roles"`
 	//是否允许所有人访问，在Permission中优先级最高
-	AllowAnyone bool `json:"allow_anyone"`
+	AllowAnyone bool `yaml:"allow_anyone"`
 }
