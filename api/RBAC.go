30a3d0da2f47e7a25a0f48d694e64b8384098a68
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-14 20:34:11 +0800

    feat:新增校验权限的接口

---
+++ api/RBAC.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"course-selection/service"
+	"course-selection/tool"
+	"github.com/gin-gonic/gin"
+	"github.com/storyicon/grbac"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func Authorization(ctx *gin.Context) {
+	//获取用户账号
+	userNumber := ctx.PostForm("userNumber")
+	//获取角色权限等级
+	err, roleLevel := service.SelectRoleLevel(userNumber)
+	roles := strings.Fields(roleLevel)
+	if err != nil {
+		tool.Failure(ctx, 500, "服务器错误")
+		log.Fatal("查询用户等级错误", err)
+		return
+	}
+	//通过权限等级获取权限（解析yaml文件中的权限配置
+	//判断角色是否有权限
+	//以一分钟的频率获取最新的身份
+	rbac, err := grbac.New(grbac.WithLoader(service.ParseYaml, time.Minute))
+	if err != nil {
+		tool.Failure(ctx, 400, "你还没有这个权限哦")
+		log.Fatal("解析权限配置文件错误", err)
+		return
+	}
+	state, err := rbac.IsRequestGranted(ctx.Request, roles)
+	if err != nil {
+		tool.Failure(ctx, 500, "服务器错误")
+		log.Fatal("查询用户等级错误", err)
+		return
+	}
+	if !state.IsGranted() {
+		tool.Failure(ctx, http.StatusUnauthorized, "未满十八岁🈲止访问")
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+	}
+
+}
