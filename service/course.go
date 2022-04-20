e7c0588591615dce0230e5c7698b045efedad232
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-20 18:33:22 +0800

    feat:新增将课程信息存入redis的功能

--- service/course.go
+++ service/course.go
@@ -5,8 +5,21 @@ import (
 	"course-selection/model"
 )
 
-// InsertCourse 插入新的课程信息
+// InsertCourse mysql插入新的课程信息
 func InsertCourse(course model.Course) error {
 	err := dao.InsertCourse(course)
+	return err
+}
+
+// RInsertCourse redis插入新的课程信息
+func RInsertCourse(course model.Course) error {
+	courseMap := make(map[string]interface{})
+	courseMap["courseName"] = course.CourseName
+	courseMap["courseDepartment"] = course.CourseDepartment
+	courseMap["courseCredit"] = course.CourseCredit
+	courseMap["courseType"] = course.CourseType
+	courseMap["setTime"] = course.SetTime
+	courseMap["duration"] = course.Duration
+	err := dao.HashSet(course.CourseNumber, courseMap)
 	return err
 }
