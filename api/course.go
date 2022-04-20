34930f526bb84fbd92a7e394d1e7bcd3336b626b
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-20 19:53:01 +0800

    feat:新增查询所有课程信息的接口

--- api/course.go
+++ api/course.go
@@ -4,36 +4,15 @@ import (
 	"course-selection/model"
 	"course-selection/service"
 	"course-selection/tool"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"strconv"
 )
 
 //插入新的课程信息
 func insertCourse(ctx *gin.Context) {
-	//检验登录状态
-	//确认登录状态
-	tokenString := ctx.Request.Header.Get("token")
-	tokenClaims, err := service.ParseToken(tokenString)
-	if err != nil {
-		fmt.Println("token解析失败", err)
-		tool.Failure(ctx, 500, "服务器错误")
-		return
-	}
-	_, flag, err := service.Get(tokenClaims.UserId)
-	if err != nil {
-		fmt.Println("查询统一验证码错误", err)
-		tool.Failure(ctx, 500, "服务器错误")
-		return
-	}
-	if !flag {
-		tool.Failure(ctx, 400, "token不存在")
-		return
-	}
-
 	courseNumber := ctx.PostForm("courseNumber")
 	//课程编号单独存入redis
-	err = service.SetAdd("course", courseNumber)
+	err := service.SetAdd("course", courseNumber)
 	tool.DealWithErr(ctx, err, "插入课程编号出错")
 	courseName := ctx.PostForm("courseName")
 	courseDepartment := ctx.PostForm("courseDepartment")
@@ -72,6 +51,26 @@ func insertCourse(ctx *gin.Context) {
 	tool.DealWithErr(ctx, err, "课程信息存入redis出错")
 }
 
-func getCourseInfo(ctx *gin.Context) {
-
+func getAllCourse(ctx *gin.Context) {
+	err, members := service.SetGet("course")
+	tool.DealWithErr(ctx, err, "从redis中获取课程编号错误")
+	var courseDetails model.Course
+	var courseDetailsArr []model.Course
+	for _, val := range members {
+		err, result := service.HashGetAll(val)
+		tool.DealWithErr(ctx, err, "查询课程详情出错")
+		courseDetails.CourseNumber = result["courseNumber"]
+		courseDetails.CourseName = result["courseName"]
+		courseDetails.CourseDepartment = result["courseDepartment"]
+		credit, err := strconv.ParseFloat(result["courseCredit"], 64)
+		tool.DealWithErr(ctx, err, "学分string转为float出错")
+		courseDetails.CourseCredit = credit
+		classType, err := strconv.Atoi(result["courseType"])
+		tool.DealWithErr(ctx, err, "课程类型string转为int出错")
+		courseDetails.CourseType = classType
+		courseDetails.SetTime = result["setTime"]
+		courseDetails.Duration = result["duration"]
+		courseDetailsArr = append(courseDetailsArr, courseDetails)
+	}
+	tool.Success(ctx, 200, courseDetailsArr)
 }
