6014347dc3eb5acda8cff7527368dfed1a4d49f0
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-21 15:21:21 +0800

    refactor:修改存储在MySQL中的课程信息

--- dao/course.go
+++ dao/course.go
@@ -3,7 +3,7 @@ package dao
 import "course-selection/model"
 
 func InsertCourse(course model.Course) error {
-	result := DB.Select("courseNumber", "courseName", "courseDepartment", "courseCredit", "courseType", "teacher", "teachingClass", "courseGrade", "duration").Create(&course)
+	result := DB.Select("courseNumber", "courseName", "courseDepartment", "courseCredit", "courseType", "courseGrade", "duration").Create(&course)
 	return result.Error
 }
 
