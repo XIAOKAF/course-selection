b5368eb6550fd01b68c4fbe35582017f205183d4
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-21 15:21:47 +0800

    refactor:重新定义课程的结构体

--- model/course.go
+++ model/course.go
@@ -7,10 +7,8 @@ type Course struct {
 	CourseDepartment string
 	CourseCredit     float64
 	CourseType       int
-	Teacher          string
 	TeachingClass    string
 	CourseGrade      string
-	SetTime          string
 	Duration         string
 }
 
