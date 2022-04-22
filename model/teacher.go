0d35d27c467e21fdfd01eaf2d6efdcc6491184ac
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-22 11:22:58 +0800

    refactor:定义教师结构体

---
+++ model/teacher.go
@@ -0,0 +1,12 @@
+package model
+
+type Teacher struct {
+	TeacherId   int
+	WorkNumber  string
+	TeacherName string
+	Gender      int
+	Password    string
+	Department  string
+	JobTitle    string
+	RuleLevel   int
+}
