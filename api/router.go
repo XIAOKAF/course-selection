1877f2763648d531d70bcc8cd2142472ccb7b5f3
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-22 21:31:37 +0800

    refactor:规范代码

--- api/router.go
+++ api/router.go
@@ -6,36 +6,46 @@ import (
 
 func InitEngine() {
 	engine := gin.Default()
+
 	engine.Use(Authorization)
-	engine.POST("/checkSms", checkSms) //检验验证码是否正确
-	//高级管理员
+
+	//短信接口
+	SmsGroup := engine.Group("/Sms")
+	{
+		SmsGroup.POST("/sendSms", sendSms)   //发送短信验证码
+		SmsGroup.POST("/checkSms", checkSms) //校验短信验证码
+	}
+
+	//管理员接口
 	administratorGroup := engine.Group("/administrator")
 	{
 		administratorGroup.Use(parseToken)                    //解析token
 		administratorGroup.POST("/login", administratorLogin) //管理员登录
 		administratorGroup.POST("/cancel", cancel)            //注销学生账号
 	}
-	//学生
-	engine.POST("/studentRegister", studentRegister) //学生注册（类似于学生第一天报道后激活官方账号
+
+	//学生接口
+	engine.POST("/studentRegister", studentRegister) //学生注册
+	engine.POST("/studentLogin", studentLogin)       //密码登录
 	studentGroup := engine.Group("/student")
 	{
 		studentGroup.Use(parseToken)                                 //解析token
-		studentGroup.POST("/loginByVerifyCode", sendSms)             //短信登录
-		studentGroup.POST("/studentLogin", studentLogin)             //密码登录
 		studentGroup.POST("/changePassword", changePwdByOldPwd)      //通过旧密码修改密码
 		studentGroup.POST("/updateMobile", updateMobile)             //更新电话号码
 		studentGroup.POST("/checkCodeForUpdate", checkCodeForUpdate) //更新电话号码时校验验证码
 		studentGroup.POST("/updateAvatar", updateAvatar)             //更新头像
 		studentGroup.GET("/selectInfo", selectInfo)                  //查询学生信息
 	}
-	//教师
+
+	//教师接口
+	engine.POST("/teacherLogin", teacherLogin) //教师登录
 	teacherGroup := engine.Group("/teacher")
 	{
-		teacherGroup.POST("/login", teacherLogin)                //教师登录
 		teacherGroup.GET("/getTeachingClass", getTeachingClass)  //获取所有教学班
 		teacherGroup.GET("/studentSelection", studentsSelection) //获取选择教学班的同学信息
 	}
-	//课程
+
+	//课程接口
 	courseGroup := engine.Group("/course")
 	{
 		courseGroup.POST("/insertCourse", createCurriculum)      //开设新的课程
