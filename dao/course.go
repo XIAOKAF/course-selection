a4e739fc37370451147cf060342704de366d1688
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-04-23 09:56:58 +0800

    refactor:删除无效代码

--- dao/course.go
+++ dao/course.go
@@ -2,12 +2,6 @@ package dao
 
 import "course-selection/model"
 
-func SelectCourse(courseNumber string) error {
-	var courseName string
-	result := DB.Table("course").Select("courseName").Scan(&courseName)
-	return result.Error
-}
-
 func CreateCourse(course model.Course) error {
 	result := DB.Select("courseNumber", "courseName", "courseDepartment", "courseCredit", "courseType", "courseGrade", "duration").Create(&course)
 	return result.Error
