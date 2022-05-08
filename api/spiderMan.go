8624bc18f5aa4336d2d368c49f209360607c4383
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-05-08 19:34:33 +0800

    feat:新增获取学生信息的接口

--- api/spiderMan.go
+++ api/spiderMan.go
@@ -27,7 +27,7 @@ func spiderMan(ctx *gin.Context) {
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
 	req.Header.Set("Referer", "http://jwzx.cqupt.edu.cn/kebiao/kb_stu.php?xh=2021212196")
 	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")
-	req.Header.Set("cookie", "mLvnBZTNP4mtS=565PBR7jNqVZ5YjbZ63CBI9r1yzWMqDldJPIZW5JwfGupX9UaXLaXP4HRep2I7vGK8FU65eSLzC_9OLAIcYIRBq; PHPSESSID=ST-1068784-Q9AAJNzmPAxgNQ-zKsUuEjVzSiQauthserver1; mLvnBZTNP4mtT=kyZvtP4.CQl7aJLMPUHHmfF965j3JBZfBlZPwQDxm1dcVcYEICf_yaORLz9Ynuo5BFkXlYoO_fr8YO_qtBodkfIQxo7V2OlW.f9TOmofLrkSO36Ui2eWQK0SS5rWvHvbYPtt8Tj0WDS095nEz0R5G2Flb9dZ.8M8bDVpiAizVB1G8TWo7pSIC1vMrgL6n2rfqa7dPtxdEBNjcavcqM2y6O20K0UNpnehriVmh6ZC4a.iYS_7VFV_PRT_n8kCARI.TKACepeTCHSynaQSn9FTFuxDH3pYKEpHE669RJkfUnRbLgoUX2nJ6IgacpXOyHbcoviwCb4VsKk4KqAEq3MUarU2VsI9ld3LTkgsDdbJoQ9")
+	req.Header.Set("cookie", "mLvnBZTNP4mtS=565PBR7jNqVZ5YjbZ63CBI9r1yzWMqDldJPIZW5JwfGupX9UaXLaXP4HRep2I7vGK8FU65eSLzC_9OLAIcYIRBq; PHPSESSID=ST-401632-ej2QGXjh6MBlvN0FecdEHagCJzEauthserver2; mLvnBZTNP4mtT=yTO6AhGoqE1gTIvi2OJmhMq0zGBBDf5Xlia5my6G1b.bv2_iLSmUX82X.jIKr3_WORrf2gnnQACjHkn9GHNTZj1R5NnlyblP8dQxoxplF_xl_zeRyvl7BQ.cTd2wGI7vuNHgk3Am6Oveqe7XMlAteFJ0MrdUnk8iT8sHbU9dP_JCYdQLNhfRtTqNK1kZR9D072zCzJNWOgvUvR_6Xf2qKm_kFIMsmX0FGLkxfaG22fKR27jh0CiXP5FJZmz4ivLwUElRs8zzbE.MtuKhx3DVKcXKrP3H4__YJEZ518wcbFKGu0.SJBkc2jfJmO5YaKy9aZjAUqzdYtP4h.inSZQ5VbMXvTfW4KjBHqzvqXVK0p4gyuK2x9Q1p3isni6h_bbRhAGfKHqJARhFupiYr8vl5a")
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("网络请求失败", err)
@@ -42,19 +42,23 @@ func spiderMan(ctx *gin.Context) {
 		tool.Failure(ctx, 500, "服务器错误")
 		return
 	}
-	student := model.Student{}
+	student := model.Student{
+		RuleId: "1",
+	}
 	for j := 1; j <= 128; j++ {
 		//#stuListTabs-current > table > tbody > tr:nth-child(1)  所有信息
+		//#stuListTabs-current > table > tbody
 		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(2)  学号
 		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(3)  姓名
 		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(4)  性别
-		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(7)  班级
-		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(8)	专业
-		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(9)	院系
+		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(9)  年级
+		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(5)  班级
+		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(7)	专业
+		//#stuListTabs-current > table > tbody > tr:nth-child(1) > td:nth-child(8)	院系
 		k := strconv.Itoa(j)
 		docDetails.Find("#stuListTabs-current > table > tbody > tr:nth-child(" + k + ")").
 			Each(func(i int, s *goquery.Selection) {
-				student.StudentId = j
+				student.StudentId = s.Find("td:nth-child(2)").Text()
 				student.StudentName = s.Find("td:nth-child(3)").Text()
 				student.Password, err = pinyin.New(student.StudentName).Split("").Mode(pinyin.WithoutTone).Convert()
 				if err != nil {
@@ -68,12 +72,61 @@ func spiderMan(ctx *gin.Context) {
 				} else {
 					student.Gender = 1
 				}
-				student.Class = s.Find("td:nth-child(7)").Text()
-				student.Major = s.Find("td:nth-child(8)").Text()
-				student.Department = s.Find("td:nth-child(9)").Text()
+				student.Class = s.Find("td:nth-child(5)").Text()
+				student.Major = s.Find("td:nth-child(7)").Text()
+				student.Department = s.Find("td:nth-child(8)").Text()
+				student.Grade = s.Find("td:nth-child(9)").Text()
 				err = service.SpiderMan(student)
 				if err != nil {
 					fmt.Println("将学生信息导入MySQL失败", err)
+					tool.Failure(ctx, 500, "服务器错误")
+					return
+				}
+				err = service.HashSet("student", student.StudentId, "12345678900")
+				if err != nil {
+					fmt.Println("储存学生信息失败")
+					tool.Failure(ctx, 500, "服务器错误")
+					return
+				}
+				err = service.HashSet(student.StudentId, "studentName", student.Password)
+				if err != nil {
+					fmt.Println("储存学生姓名失败")
+					tool.Failure(ctx, 500, "服务器错误")
+					return
+				}
+				err = service.HashSet(student.StudentId, "password", student.Password)
+				if err != nil {
+					fmt.Println("储存学生密码失败")
+					tool.Failure(ctx, 500, "服务器错误")
+					return
+				}
+				err = service.HashSet(student.StudentId, "gender", sex)
+				if err != nil {
+					fmt.Println("储存学生性别失败")
+					tool.Failure(ctx, 500, "服务器错误")
+					return
+				}
+				err = service.HashSet(student.StudentId, "class", student.Class)
+				if err != nil {
+					fmt.Println("储存学生班级失败")
+					tool.Failure(ctx, 500, "服务器错误")
+					return
+				}
+				err = service.HashSet(student.StudentId, "department", student.Department)
+				if err != nil {
+					fmt.Println("储存学生院系失败")
+					tool.Failure(ctx, 500, "服务器错误")
+					return
+				}
+				err = service.HashSet(student.StudentId, "major", student.Major)
+				if err != nil {
+					fmt.Println("储存学生专业失败")
+					tool.Failure(ctx, 500, "服务器错误")
+					return
+				}
+				err = service.HashSet(student.StudentId, "ruleId", "1")
+				if err != nil {
+					fmt.Println("储存学生权限等级错误", err)
 					tool.Failure(ctx, 500, "服务器错误")
 					return
 				}
