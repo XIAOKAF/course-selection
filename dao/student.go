edaa72a57fea302daf6e29a5e92572d1b8dd4ddb
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-05-08 21:35:29 +0800

    del:删除更新MySQL学生密码与电话号码的功能

--- dao/student.go
+++ dao/student.go
@@ -6,20 +6,3 @@ func SpiderMan(student model.Student) error {
 	result := DB.Select("student_id", "student_name", "gender", "grade", "class", "department", "major", "rule_id").Create(&student)
 	return result.Error
 }
-
-func SelectUnifiedCode(unifiedCode string) (error, string) {
-	var pwd string
-	result := DB.Select("student").Where("unifiedCode = ?", unifiedCode).Take(&pwd)
-	return result.Error, pwd
-}
-
-// UpdatePassword 修改密码
-func UpdatePassword(student model.Student) error {
-	result := DB.Model(&student).Where("unifiedCode = ?", student.UnifiedCode).Update("password", student.Password)
-	return result.Error
-}
-
-func UpdateMobile(student model.Student) error {
-	result := DB.Model(&student).Where("unifiedCode = ?", student.UnifiedCode).Update("mobile", student.Mobile)
-	return result.Error
-}
--- service/student.go
+++ service/student.go
@@ -3,38 +3,8 @@ package service
 import (
 	"course-selection/dao"
 	"course-selection/model"
-	"gorm.io/gorm"
 )
 
 func SpiderMan(student model.Student) error {
 	return dao.SpiderMan(student)
-}
-
-func SelectUnifiedCode(unifiedCode string) (bool, error, string) {
-	err, pwd := dao.SelectUnifiedCode(unifiedCode)
-	if err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return false, nil, pwd
-		}
-		return true, err, pwd
-	}
-	return true, nil, pwd
-}
-
-// UpdatePassword 更改用户密码
-func UpdatePassword(student model.Student) error {
-	err := dao.UpdatePassword(student)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// UpdateMobile 更新用户电话号码
-func UpdateMobile(student model.Student) error {
-	err := dao.UpdateMobile(student)
-	if err != nil {
-		return err
-	}
-	return nil
 }
