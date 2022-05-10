6e75e4da801e64440adea23533031753cd6ec2b7
XIAOKAF <2021212196@stu.cqupt.edu.cn>
2022-05-10 19:08:01 +0800

    fix:修复短信配置错误的bug

--- model/SMS.go
+++ model/SMS.go
@@ -1,11 +1,11 @@
 package model
 
 type Message struct {
-	SecretId   string `yaml:"secret_id"`
-	SecretKey  string `yaml:"secret_key"`
-	AppId      string `yaml:"app_id"`
-	AppKey     string `yaml:"app_key"`
-	SignId     string `yaml:"sign_id"`
-	TemplateId string `yaml:"template_id"`
-	Sign       string `yaml:"sign"`
+	SecretId   string `json:"secret_id"`
+	SecretKey  string `json:"secret_key"`
+	AppId      string `json:"app_id"`
+	AppKey     string `json:"app_key"`
+	SignId     string `json:"sign_id"`
+	TemplateId string `json:"template_id"`
+	Sign       string `json:"sign"`
 }
